73da151eba236fcee73c3ec1d332726171866019
Andrew Apodaca <andy.apod@gmail.com>
2025-12-08 21:43:21 -0600

    adds fall back image

--- script.js
+++ script.js
@@ -9,4 +9,13 @@ const baseURL = `https://www.DenariOnline.com/donorservices/viewSQLPhoto.aspx`;
 const imageURL = baseURL + queryParams;
 
 const imageElement = document.getElementById("image");
+
+imageElement.onerror = function () {
+  console.log("Image failed to load. Switching to placeholder.");
+  this.src =
+    "https://media.istockphoto.com/id/1223671392/vector/default-profile-picture-avatar-photo-placeholder-vector-illustration.jpg?s=612x612&w=0&k=20&c=s0aTdmT5aU6b8ot7VKm11DeID6NctRCpB755rA1BIP0="; // Your backup image
+
+  this.onerror = null;
+};
+
 imageElement.setAttribute("src", imageURL);
